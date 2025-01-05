c2ed050b7b7d18262928b1be8f7fddb9ea2197de
{zufichris} <{zufichris@gmail.com}>
2025-01-05 22:41:07 +0400

    Root loader

---
+++ app/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function Loading() {
+  return (
+    <div className="py-12 md:py-24 lg:py-32">
+      <div className="space-y-10">
+        <div className="space-y-4">
+          <Skeleton className="h-12 w-[50%] mx-auto" />
+          <Skeleton className="h-4 w-[80%] mx-auto" />
+        </div>
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {Array(6).fill(null).map((_, i) => (
+            <div key={i} className="space-y-4">
+              <Skeleton className="h-[200px] w-full" />
+              <Skeleton className="h-4 w-[80%]" />
+              <Skeleton className="h-4 w-[60%]" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
