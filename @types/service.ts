88e6ef56535f2d1ada97f9c954c45c48cda8cf52
{zufichris} <{zufichris@gmail.com}>
2025-01-07 01:38:52 +0400

    Added @types service

---
+++ @types/service.ts
@@ -0,0 +1,11 @@
+export interface IService {
+    title: string,
+    slug: string,
+    description: string,
+    icon: string,
+    image: string,
+    technologies: string[],
+    features: string[],
+    benefits: string[],
+    process: string[]
+}
